55625097f08b1ecc8d220b087010325abd3c4ca3
Alejandro <almillan@ucsc.edu>
2017-06-11 22:17:25 -0700

    Added RGB to courses in AppInfo

--- app/src/main/java/com/example/alex/studybuddies/AppInfo.java
+++ app/src/main/java/com/example/alex/studybuddies/AppInfo.java
@@ -21,6 +21,16 @@ public class AppInfo {
     // Here are some values we want to keep global.
     public static ArrayList<HashMap<String, String>> courses = new ArrayList<>();
     public static ArrayList<HashMap<String, String>> coursesJoined = new ArrayList<>();
+    private final static String KEY_COURSE= "course";
+    private final static String KEY_RED = "red";
+    private final static String KEY_BLUE = "blue";
+    private final static String KEY_GREEN = "green";
+    private final static String KEY_COURSE_SIZE = "SavedSize";
+
+    private final static String KEY_JOINED= "course_joined";
+    private final static String KEY_POSITION = "position";
+    private final static String KEY_TIME = "time";
+    private final static String KEY_JOINED_SIZE = "JoinedSize";
 
 
     private Context my_context;
@@ -30,27 +40,30 @@ public class AppInfo {
             instance = new AppInfo();
             instance.my_context = context;
             SharedPreferences settings = context.getSharedPreferences(MainActivity.MYPREFS, 0);
-            int size = settings.getInt("SavedSize", 0);
+            int size = settings.getInt(KEY_COURSE_SIZE, 0);
             for (int i = 0; i < size ; i++) {
-                String myCourse = settings.getString("course"+i, "null");
-                String pos = settings.getString("position"+i, "null");
-                String time = settings.getString("time"+i, "null");
-                HashMap<String, String> tempClass = new HashMap<>();
-                tempClass.put("class", myCourse);
-                tempClass.put("position", pos);
-                tempClass.put("time", time);
-                instance.coursesJoined.add(tempClass);
+                String myCourse = settings.getString(KEY_COURSE + i, "null");
+                String red = settings.getString(KEY_RED + i, "null");
+                String blue = settings.getString(KEY_BLUE + i, "null");
+                String green = settings.getString(KEY_GREEN + i, "null");
+
+                HashMap<String, String> temp = new HashMap<>();
+                temp.put(KEY_COURSE, myCourse);
+                temp.put(KEY_RED, red);
+                temp.put(KEY_BLUE, blue);
+                temp.put(KEY_GREEN, green);
+                instance.courses.add(temp);
             }
 
-            int size2 = settings.getInt("HashSize", 0);
+            int size2 = settings.getInt(KEY_JOINED_SIZE, 0);
             for (int i = 0; i < size2 ; i++) {
-                String course = settings.getString("course"+i, "null");
-                String pos = settings.getString("position"+i, "null");
-                String time = settings.getString("time"+i, "null");
+                String course = settings.getString(KEY_JOINED+i, "null");
+                String pos = settings.getString(KEY_POSITION+i, "null");
+                String time = settings.getString(KEY_TIME+i, "null");
                 HashMap<String, String> tempClass = new HashMap<>();
-                tempClass.put("class", course);
-                tempClass.put("position", pos);
-                tempClass.put("time", time);
+                tempClass.put(KEY_JOINED, course);
+                tempClass.put(KEY_POSITION, pos);
+                tempClass.put(KEY_TIME, time);
                 instance.coursesJoined.add(tempClass);
             }
 
@@ -58,32 +71,38 @@ public class AppInfo {
         return instance;
     }
 
-    public void addClass(String c) {
+    public void addClass(String myCourse, String red, String green, String blue) {
         int i = instance.courses.size()-1;
         SharedPreferences settings = my_context.getSharedPreferences(MainActivity.MYPREFS, 0);
         SharedPreferences.Editor editor = settings.edit();
-        if(i < 0){
-            i = 0;
-        }
-        editor.putString(courses.get(i), c);
-        instance.courses.add(c);
-        editor.putInt("SavedSize", instance.getSize());
+
+        //Saves the data into the phone even after we close the app
+        editor.putString(KEY_COURSE + i, myCourse);
+        editor.putString(KEY_RED + i, red);
+        editor.putString(KEY_GREEN + i, green);
+        editor.putString(KEY_BLUE + i, blue);
+
+        //Stores the data into the current loaded app
+        HashMap<String, String> temp = new HashMap<>();
+        temp.put(KEY_COURSE, myCourse);
+        temp.put(KEY_RED, myCourse);
+        temp.put(KEY_GREEN, green);
+        temp.put(KEY_BLUE, blue);
+        instance.courses.add(temp);
+        editor.putInt(KEY_COURSE_SIZE, instance.getSize());
         editor.commit();
     }
-
-    public void addStudyGroup(String c, String p, String t) {
-        int i = instance.coursesJoined.size() - 1;
+    public void delete(int i){
         SharedPreferences settings = my_context.getSharedPreferences(MainActivity.MYPREFS, 0);
         SharedPreferences.Editor editor = settings.edit();
-        editor.putString("course"+i, c);
-        editor.putString("position" + i, p);
-        editor.putString("time" + i, t);
-        HashMap<String, String> tempClass = new HashMap<>();
-        tempClass.put("class", c);
-        tempClass.put("position", p);
-        tempClass.put("time", t);
-        instance.coursesJoined.add(tempClass);
-        editor.putInt("HashSize", instance.getHashSize());
+        //i++
+        editor.remove(KEY_COURSE + i);
+        editor.remove(KEY_RED + i);
+        editor.remove(KEY_GREEN + i);
+        editor.remove(KEY_BLUE + i);
+
+        instance.courses.remove(i);
+        editor.putInt(KEY_COURSE_SIZE, instance.getSize());
         editor.commit();
     }
 
@@ -91,29 +110,38 @@ public class AppInfo {
         return courses.size();
     }
 
+    public void addStudyGroup(String c, String p, String t) {
+        int i = instance.coursesJoined.size() - 1;
+        SharedPreferences settings = my_context.getSharedPreferences(MainActivity.MYPREFS, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(KEY_JOINED + i, c);
+        editor.putString(KEY_POSITION + i, p);
+        editor.putString(KEY_TIME + i, t);
+        HashMap<String, String> tempClass = new HashMap<>();
+        tempClass.put(KEY_JOINED, c);
+        tempClass.put(KEY_POSITION, p);
+        tempClass.put(KEY_TIME, t);
+        instance.coursesJoined.add(tempClass);
+        editor.putInt(KEY_JOINED_SIZE, instance.getHashSize());
+        editor.commit();
+    }
+
+
     public int getHashSize(){
         return coursesJoined.size();
     }
 
-    public void delete(int i){
-        SharedPreferences settings = my_context.getSharedPreferences(MainActivity.MYPREFS, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.remove(courses.get(i));
-        instance.courses.remove(i);
-        editor.putInt("SavedSize", instance.getSize());
-        editor.commit();
 
-    }
 
     public void deleteHash(int i){
         SharedPreferences settings = my_context.getSharedPreferences(MainActivity.MYPREFS, 0);
         SharedPreferences.Editor editor = settings.edit();
         i++;
-        editor.remove("course"+i);
-        editor.remove("position" + i);
-        editor.remove("time" + i);
+        editor.remove(KEY_JOINED + i);
+        editor.remove(KEY_POSITION + i);
+        editor.remove(KEY_TIME + i);
         instance.coursesJoined.remove(i);
-        editor.putInt("HashSize", instance.getHashSize());
+        editor.putInt(KEY_JOINED_SIZE, instance.getHashSize());
         editor.commit();
 
     }
